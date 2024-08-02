13f7e31a3fc0a8601f8b54d3281ff8e793a49a84
Samira Rodrigues <samira.silva@zup.com.br>
2024-08-02 14:14:10 -0300

    add menu

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,51 @@ import java.util.*;
 //Estrutura QuickSort
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         //Parte da jay
         List<Integer> numbersToSort = Arrays.asList( //Geramento do vetor comos numeros
-         4, 3, 2, 5, 1, 3, 2, 9, 0, 2);
+                12, 47, 5, 23, 38, 1, 14, 50, 32, 9,
+                45, 7, 19, 26, 36, 10, 28, 3, 34, 22,
+                4, 43, 16, 27, 48, 8, 31, 11, 25, 39,
+                18, 46, 6, 29, 40, 2, 35, 21, 44, 13,
+                17, 49, 20, 33, 15, 37, 30, 41, 24, 42);
 
-        System.out.println(numbersToSort);
-        quicksort(numbersToSort, 0, numbersToSort.size() - 1);
-        System.out.println(numbersToSort);
+        boolean control = true;
+        while (control) {
+            showMenu();
+            int menu = getUserMenu();
+            if (menu == 1) {
+                quicksort(numbersToSort, 0, numbersToSort.size() - 1);
+                System.out.println(numbersToSort);
+            } else if (menu == 2) {
+                Collections.reverse(numbersToSort);
+                System.out.println(" " + numbersToSort);
+            } else if (menu == 3) {
+                System.out.println("Fim do programa.");
+                control = false;
+            } else {
+                System.out.println("Opção invalida.");
+            }
+        }
+    }
+
+    private static void showMenu() {
+        System.out.println("Escolha sua opção: \n" +
+                "1 para ver ordenar lista \n" +
+                "2 para ver lista ao contrario \n" +
+                "3 para sair \n");
+    }
+
+    private static int getUserMenu() {
+        System.out.println("Escolha sua opção: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        return scanner.nextInt();
     }
 
     public static int partition(List<Integer> numbersToSort, int begin, int end) { // numbersToSort = lista de numeros, begin = inicial, end= final
